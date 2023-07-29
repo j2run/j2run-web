7a9c30e6c1f31e20c49b100efb615c21e786f97b
wawahuy <kakahuy99@gmail.com>
2023-07-29 19:00:34 +0700

    feat: add layout create cloud

--- apps/frontend/src/pages/CreateCloud.vue
+++ apps/frontend/src/pages/CreateCloud.vue
@@ -1,3 +1,103 @@
 <template>
-  <div>Hello world</div>
+  <v-row class="ma-2">
+    <v-col cols="9">
+      <v-card
+        class="mx-auto border"
+        elevation="2"
+        rounded="lg"
+      >
+        <v-card-title class="mt-2">Tạo mới cloud</v-card-title>
+
+        <v-card-item>
+          <v-row>
+            <v-col>
+              <div class="text-subtitle-1 text-medium-emphasis mb-1">Plan Cloud</div>
+              <v-select
+                variant="outlined"
+                density="compact"
+                style="max-width: 500px;"
+                prepend-inner-icon="mdi-package-variant"
+              ></v-select>
+            </v-col>
+
+            <v-col>
+              <div class="text-subtitle-1 text-medium-emphasis mb-1">Thời gian</div>
+              <v-select
+                variant="outlined"
+                density="compact"
+                style="max-width: 300px;"
+                prepend-inner-icon="mdi-clock-star-four-points-outline"
+              ></v-select>
+            </v-col>
+          </v-row>
+
+          <v-card
+            class="mb-6"
+            color="surface-variant"
+            variant="tonal"
+          >
+            <v-card-text class="text-medium-emphasis text-caption">
+              Cảnh báo: Hiện tại bạn sẽ không thể thay đổi game sau khi nhấn 'tạo mới'
+            </v-card-text>
+          </v-card>
+
+          <div class="text-subtitle-1 text-medium-emphasis mb-1">Tên Cloud</div>
+          <v-text-field
+            variant="outlined"
+            density="compact"
+            style="max-width: 500px;"
+            prepend-inner-icon="mdi-rename-box"
+          ></v-text-field>
+
+          <div class="text-subtitle-1 text-medium-emphasis mb-1">Game</div>
+          <v-select
+            variant="outlined"
+            density="compact"
+            style="max-width: 500px;"
+            prepend-inner-icon="mdi-gamepad-variant-outline"
+          ></v-select>
+        </v-card-item>
+
+      </v-card>
+    </v-col>
+    <v-col cols="3">
+      <v-card
+        class="mx-auto border"
+        elevation="2"
+        rounded="lg"
+      >
+        <v-card-title class="mt-2">Hóa đơn</v-card-title>
+
+        <v-card-text>
+          <div class="mt-2">
+            <span class="text-subtitle-2">Dịch vụ:</span>
+            <span class="ml-2 font-weight-bold">Gói 1</span>
+          </div>
+          <div class="mt-2 mb-3">
+            <span class="text-subtitle-2">Giá:</span>
+            <span class="ml-2 font-weight-bold">20.000 vnđ</span>
+          </div>
+          <div class="mt-2 mb-3">
+            <span class="text-subtitle-2">Thời gian:</span>
+            <span class="ml-2 font-weight-bold">3 tháng</span>
+          </div>
+          <v-divider></v-divider>
+          <div class="mt-2 text-h5 text-right">
+            60.000 vnđ
+          </div>
+        </v-card-text>
+
+        <v-card-actions class="ma-3 mt-0">
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="primary"
+            variant="outlined"
+          >
+            Tạo mới
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
